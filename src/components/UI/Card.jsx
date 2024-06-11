173831451f2a8b6f46aadc9bc7c46669ca5c6aac
Yağız Can Dinç <ycandiinc@gmail.com>
2024-06-11 19:57:14 +0300

    Card componenti tasarlandı

---
+++ src/components/UI/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//propsChildren ile Card ile sarmaladığımızı aldık
+const Card = (props) => {
+  return (
+    <div className={` bg-white p-5 rounded-xl ${props.className}`}>
+        {props.children}
+    </div>
+  )
+}
+
+export default Card
---
+++ src/components/Workers/AddWorker.jsx
@@ -0,0 +1,32 @@
+import Card from "../UI/Card";
+const AddWorker = () => {
+  return (
+    <Card className=' mt-10'>
+      <form className="flex flex-col gap-y-2">
+        <label htmlFor="name" className="font-medium">
+          Çalışan İsmi
+        </label>
+        <input
+          type="text"
+          className="max-w-[40rem] w-full mx-auto border p-2"
+          placeholder="Çalışan ismi yazınız"
+          id="name"
+        />
+        <label htmlFor="wage" className="font-medium">
+          Maaş Miktarı
+        </label>
+        <input
+          type="text"
+          className="max-w-[40rem] w-full mx-auto border p-2"
+          placeholder="Maaş miktarı yazınız"
+          id="wage"
+        />
+        <button className="p-2 text-lg bg-teal-700 text-white mt-2">
+          Ekle
+        </button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddWorker;
