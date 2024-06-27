e93974d38e50348f7ef67505e9da2a5dbeff7025
Yağız Can Dinç <ycandiinc@gmail.com>
2024-06-27 14:08:10 +0300

    useEffect ile localStorage kullanımı

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,24 @@ import AddWorker from "./components/Workers/AddWorker";
 import WorkerList from "./components/Workers/WorkerList";
 
 function App() {
-  const [workers, setWorkers] = useState([]);
+  //bununlar localStrage da veri varsa ilk değer olarak o veriyi al .parse ile JSON ı js çevir ve kullan yoksa boş dizi al
+  //sayfayı yenileyince veri kalsın diye yaptık
+  const [workers, setWorkers] = useState(localStorage.getItem("workers") 
+  ? JSON.parse(localStorage.getItem("workers")) 
+  : []
+);
 
   useEffect(() => {
-    localStorage.setItem("workers",JSON.stringify(workers))
+    localStorage.setItem("workers", JSON.stringify(workers));
   }, [workers]);
-//uygulama çalıştığında workers listesini localStrorage a attı [] depc içine workers stateini
-//yazdık ve her workers da değişiklik olduğunda çalışsın dedik
+  //hiç depc [] olmazsa hep çalışır , [] olursa bir kere [workers] ilk açıldığında ve workers değişince
+  //uygulama çalıştığında workers listesini localStrorage a attı [] depc içine workers stateini
+  //yazdık ve her workers da değişiklik olduğunda çalışsın dedik
+
+  // useEffect(() => {
+  //   console.log("run");
+  // }, []);
+  
   return (
     <React.Fragment>
       <h1 className=" text-white text-center mt-6 txt-3xl">Maaş Otomasyonu</h1>
